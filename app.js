88493a0cfa88bfa6c873ed29593f3f82990dff8a
SusieHatter <susiivaldez@gmail.com>
2021-08-14 12:46:33 -0500

    Add seconds timer functionality

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const TOTAL_MIN = 25;
-const MINUTE_VALUE = 60000;
+const TOTAL_SEC = 60;
+const MINUTE_VALUE = 1000;
+const SECOND_VALUE = 100;
+
+function createSecondTile(second) {
+  const secondTile = document.createElement("div");
+  secondTile.classList.add("second-tile");
+  setTimeout(function () {
+    secondTile.classList.add("second-tile-passed");
+  }, SECOND_VALUE * second);
+  return secondTile;
+}
 
 function createMinuteTile(minute) {
   const minuteTile = document.createElement("div");
+
+  for (let second = 1; second <= TOTAL_SEC; second++) {
+    const secondTile = createSecondTile(second);
+    minuteTile.appendChild(secondTile);
+  }
+
   minuteTile.classList.add("minute-tile");
   setTimeout(function () {
     minuteTile.classList.add("minute-tile-passed");
