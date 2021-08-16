6484f1c47dfe900d24aa18d1eb99589785fbb480
SusieHatter <susiivaldez@gmail.com>
2021-08-15 23:59:17 -0500

    Done

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const TOTAL_MIN = 25;
 const TOTAL_SEC = 60;
-const MINUTE_VALUE = 1000;
 const SECOND_VALUE = 100;
+const MINUTE_VALUE = SECOND_VALUE * 60;
 
 function createSecondTile(second) {
+  console.log(second);
   const secondTile = document.createElement("div");
   secondTile.classList.add("second-tile");
   setTimeout(function () {
+    secondTile.classList.add("second-tile-active");
+  }, SECOND_VALUE * (second - 1));
+  setTimeout(function () {
+    secondTile.classList.remove("second-tile-active");
     secondTile.classList.add("second-tile-passed");
   }, SECOND_VALUE * second);
   return secondTile;
@@ -16,12 +21,16 @@ function createMinuteTile(minute) {
   const minuteTile = document.createElement("div");
 
   for (let second = 1; second <= TOTAL_SEC; second++) {
-    const secondTile = createSecondTile(second);
+    const secondTile = createSecondTile(second + (minute - 1) * TOTAL_SEC);
     minuteTile.appendChild(secondTile);
   }
 
   minuteTile.classList.add("minute-tile");
   setTimeout(function () {
+    minuteTile.classList.add("minute-tile-active");
+  }, MINUTE_VALUE * (minute - 1));
+  setTimeout(function () {
+    minuteTile.classList.remove("minute-tile-active");
     minuteTile.classList.add("minute-tile-passed");
   }, MINUTE_VALUE * minute);
   return minuteTile;
