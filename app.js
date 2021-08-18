669c92b3dacba6f9c29bc3ea4f240155aa7764e1
SusieHatter <susiivaldez@gmail.com>
2021-08-17 23:35:10 -0500

    Add typewriter effect and sound effect

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const TOTAL_SEC = 60;
 const SECOND_VALUE = 1000;
 const MINUTE_VALUE = SECOND_VALUE * 60;
 
+const TICK = new Audio("tick.wav");
+
 function createSecondTile(second) {
-  console.log(second);
   const secondTile = document.createElement("div");
   secondTile.classList.add("second-tile");
   setTimeout(function () {
     secondTile.classList.add("second-tile-active");
   }, SECOND_VALUE * (second - 1));
   setTimeout(function () {
+    TICK.play();
     secondTile.classList.remove("second-tile-active");
     secondTile.classList.add("second-tile-passed");
   }, SECOND_VALUE * second);
@@ -19,12 +21,10 @@ function createSecondTile(second) {
 
 function createMinuteTile(minute) {
   const minuteTile = document.createElement("div");
-
   for (let second = 1; second <= TOTAL_SEC; second++) {
     const secondTile = createSecondTile(second + (minute - 1) * TOTAL_SEC);
     minuteTile.appendChild(secondTile);
   }
-
   minuteTile.classList.add("minute-tile");
   setTimeout(function () {
     minuteTile.classList.add("minute-tile-active");
@@ -36,9 +36,24 @@ function createMinuteTile(minute) {
   return minuteTile;
 }
 
-const timerEl = document.getElementById("timer");
+function wait(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
+async function typeWrite(el, text, interval) {
+  for (let i = 0; i < text.length; i++) {
+    el.innerHTML += text.charAt(i);
+    await wait(interval);
+  }
+}
+
+const timerEl = document.getElementById("timer");
 for (let minute = 1; minute <= TOTAL_MIN; minute++) {
   const minuteTile = createMinuteTile(minute);
   timerEl.appendChild(minuteTile);
 }
+
+const titleEl = document.getElementById("main-title");
+const titleText = "Time to Focus!";
+const typeInteral = 100;
+typeWrite(titleEl, titleText, typeInteral);
